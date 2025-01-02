de4345370995d085327e17768f085d433ab8d58d
Crashout <80020365+Crashoutv1@users.noreply.github.com>
2025-01-01 21:38:48 -0500

    Delete assets directory

--- assets/js/titles.js
+++
@@ -1,8 +0,0 @@
-const rrr = ["@Crashout", "owner", "@$$$", "Fuck them corny com kids", "Lmfao you faggot!"];
-let vvv = 0;
-
-function yyy() {
-    document.title = rrr[vvv];
-    vvv = (vvv + 1) % rrr.length
-}
-setInterval(yyy, 100)
